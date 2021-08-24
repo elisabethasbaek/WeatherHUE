e8259ffc99874054feffeeccbe85305cb11eb1ba
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-08-24 15:10:08 +0200

    pik

--- src/App.js
+++ src/App.js
@@ -47,54 +47,60 @@ export default function App() {
         }
     }
 
-    function cityColor(color){
-        document.querySelector(".city").style.color = color;
-    }
+    function Weather({backgroundImageSrc}){
+        function cityColor(color){
+            document.querySelector(".city").style.color = color;
+        }
+    
+        if(content.weather && (content.weather[0].id >= 200 && content.weather[0].id <= 299)){
+            backgroundImageSrc = "./ThunderStorm.jpg";
+            cityColor("#0A837F");
+            setLight(65600, 254, 180);
+        }
+        if(content.weather && (content.weather[0].id >= 300 && content.weather[0].id <= 499)){
+            backgroundImageSrc = "./Drizzle.jpg";
+            cityColor("#0A837F");
+            setLight(31000, 254, 80);
+        }
+        if(content.weather && (content.weather[0].id >= 500 && content.weather[0].id <= 599)){
+            backgroundImageSrc = "./Rainy.jpg";
+            cityColor("#0A837F");
+            setLight(47800, 254, 150);
+        }
+        if(content.weather && (content.weather[0].id >= 600 && content.weather[0].id <= 699)){
+            backgroundImageSrc = "./Snowy.jpg";
+            cityColor("#0A837F");
+            setLight(44000, 254, 254);
+        }
+        if(content.weather && (content.weather[0].id >= 700 && content.weather[0].id <= 799)){
+            backgroundImageSrc = "./Misty.jpg";
+            cityColor("#0A837F");
+            setLight(4500, 254, 190);
+        }
+        if(content.weather && content.weather[0].id === 800){
+            backgroundImageSrc = "./Clear.jpg";
+            cityColor("#FFBC11");
+            setLight(25000, 200, 150);
+        }
+        if(content.weather && (content.weather[0].id >= 800 && content.weather[0].id <= 803)){
+            backgroundImageSrc = "./ScatteredClouds.jpg";
+            cityColor("#FFBC11");
+            setLight(38000, 250, 150);
+        }
+        if(content.weather && (content.weather[0].id >= 804 && content.weather[0].id <= 810)){
+            backgroundImageSrc = "./Cloudy.jpg";
+            cityColor("#FFBC11");
+            setLight(10000, 254, 254);
+        }
 
-    if(content.weather && (content.weather[0].id >= 200 && content.weather[0].id <= 299)){
-        backgroundImageSrc = "./ThunderStorm.jpg";
-        cityColor("#0A837F");
-        setLight(65600, 254, 180);
-    }
-    if(content.weather && (content.weather[0].id >= 300 && content.weather[0].id <= 499)){
-        backgroundImageSrc = "./Drizzle.jpg";
-        cityColor("#0A837F");
-        setLight(31000, 254, 80);
-    }
-    if(content.weather && (content.weather[0].id >= 500 && content.weather[0].id <= 599)){
-        backgroundImageSrc = "./Rainy.jpg";
-        cityColor("#0A837F");
-        setLight(47800, 254, 150);
-    }
-    if(content.weather && (content.weather[0].id >= 600 && content.weather[0].id <= 699)){
-        backgroundImageSrc = "./Snowy.jpg";
-        cityColor("#0A837F");
-        setLight(44000, 254, 254);
-    }
-    if(content.weather && (content.weather[0].id >= 700 && content.weather[0].id <= 799)){
-        backgroundImageSrc = "./Misty.jpg";
-        cityColor("#0A837F");
-        setLight(4500, 254, 190);
-    }
-    if(content.weather && content.weather[0].id === 800){
-        backgroundImageSrc = "./Clear.jpg";
-        cityColor("#FFBC11");
-        setLight(25000, 200, 150);
-    }
-    if(content.weather && (content.weather[0].id >= 800 && content.weather[0].id <= 803)){
-        backgroundImageSrc = "./ScatteredClouds.jpg";
-        cityColor("#FFBC11");
-        setLight(38000, 250, 150);
-    }
-    if(content.weather && (content.weather[0].id >= 804 && content.weather[0].id <= 810)){
-        backgroundImageSrc = "./Cloudy.jpg";
-        cityColor("#FFBC11");
-        setLight(10000, 254, 254);
+        return(
+            <img src={backgroundImageSrc} alt="" className="backgroundImage"/>
+        )
     }
 
     return (
         <main className="App">
-            <img src={backgroundImageSrc} alt="" className="backgroundImage"/>
+            <Weather /> {/* we here and we stressed */}
 
             <form onSubmit={handleSubmit} className="search">
                 <label htmlFor="" className="search__label">Search for your city: </label>
@@ -104,7 +110,9 @@ export default function App() {
                 </div>
             </form>
 
-            {/* {content ? <Result content={content} /> : } */}
+            {content.length === 0
+            ? <h1>WHAZZUP BITCH</h1>
+            : <Result content={content} />}
 
             <div className="unitsButton">
                 <button className="unitsButton__metric" data-unit="metric" onClick={handleUnits}>&#186;C</button>
