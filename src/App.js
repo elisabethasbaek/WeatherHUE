099f9c4587a725d090c94358aa07392539d89451
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-08-24 11:11:38 +0200

    Yeet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import "./App.scss";
 import "./style/Search.scss";
@@ -8,19 +8,29 @@ import "./style/WeatherDetails.scss";
 
 export default function App() {
     var [content, setContent] = useState([]);
+    
+    function handleUnits(event){
+        var cookieName = "units";
+        var cookieValue = event.target.dataset.unit;
+        var exdays = 30;
+        const date = new Date();
+        date.setTime(date.getTime()  + (exdays*24*60*60*1000));
+        var expires = "expires="+date.toUTCString();
+        document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+        console.log(document.cookie);
+    }
 
     function handleSubmit(event){
         event.preventDefault();
-
+        
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.search.value}&units=metric&appid=01ef0b0a8b733220771bbe668022c1b3`)
-            .then(function (response) {
-                setContent(response.data);
-                console.log(response.data);
+        .then(function (response) {
+            setContent(response.data);
+            console.log(response.data);
         });
     }
 
     var backgroundImageSrc;
-    var cityColor = 
     function cityColor(color){
         document.querySelector(".city").style.color = color;
     }
@@ -59,7 +69,7 @@ export default function App() {
     }
 
     return (
-        <div className="App">
+        <main className="App">
             <img src={backgroundImageSrc} alt="" className="backgroundImage"/>
 
             <form onSubmit={handleSubmit} className="search">
@@ -74,10 +84,12 @@ export default function App() {
             
             <div className="weatherDetails">
                 <p className="weatherDetails__temp">{content.main?.temp}&#186;C</p>
-                <p className="weatherDetails__feelsLike">Feels like {content.main?.feels_like}&#186;C</p>
-                <p className="weatherDetails__weather">{content.weather[0]?.main}</p>
-                <p className="weatherDetails__details">{content.weather[0]?.description}</p>
+                <p className="weatherDetails__feelsLike">Feels like <span>{content.main?.feels_like}&#186;C</span></p>
+                <p className="weatherDetails__weather">{content.weather && content.weather[0].main} / {content.weather && content.weather[0].description}</p>
             </div>
-        </div>
+
+            <button className="unitsButton__metric" data-unit="metric" onClick={handleUnits}>Metric</button>
+            <button className="unitsButton__imperial" data-unit="imperial" onClick={handleUnits}>Imperial</button>
+        </main>
     );
 }
