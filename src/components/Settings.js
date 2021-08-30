70c23d74c4d489374831ab36da5f2dd9d4e843e5
Elisabeth Asbæk Jeppesen <elisabeth.asbaek@gmail.com>
2021-08-30 14:35:04 +0200

    yooooo PUSSSSHHHH

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,20 +1,39 @@
 import "../style/Settings.scss";
 
-export default function Settings(){
+export default function Settings({hidden}){
+    function close(){
+        hidden = -1000;
+    }
+
     function handleSubmit(){
 
     }
 
     return(
-        <div className="settings">
+        <div className="settings" style={{zIndex: hidden}}>
+            <button className="settings__close" onClick={close}><i className="fas fa-times"></i></button>
             <h1 className="settings__heading">Settings</h1>
 
             <form onSubmit={handleSubmit} className="settings__form">
-                <input type="text" placeholder="IP address"></input>
-                <input type="text" placeholder="Username (API key)"></input>
-                <input type="text" placeholder=""></input>
+                <div className="formPair">
+                    <label htmlFor="ip">Your IP address:</label>
+                    <input type="text" id="ip" placeholder="IP address"></input>
+                </div>
+
+                <div className="formPair">
+                    <label htmlFor="username">Your username:</label>
+                    <input type="text" id="username" placeholder="Username (API key)"></input>
+                </div>
+
+                <div className="formPair">
+                    <label htmlFor=""></label>
+                    <input type="text" id="" placeholder=""></input>
+                </div>
+
                 <button type="submit">Save</button>
             </form>
+
+            <p className="settings__anno"><span class="far fa-copyright"></span> 2021</p>
         </div>
     )
 }
